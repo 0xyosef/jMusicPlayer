cd3ef5f1fa8f4f38253576a872de2671f8a0cdb0
coddinglearn <89706606+coddinglearn@users.noreply.github.com>
2022-03-22 02:06:51 +0200

    Add buttom Stop

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,11 @@
 package com.company;
+
+import java.net.MalformedURLException;
+
 public class Main {
 
-    public static void main(String[] args)  {
-        WavPlayer t = new WavPlayer("1.wav");
+    public static void main(String[] args) {
+        WavPlayer t = new WavPlayer();
         t.setVisible(true);
     }
 }
--- src/com/company/WavPlayer.java
+++ src/com/company/WavPlayer.java
@@ -4,7 +4,6 @@ import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,32 +11,35 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class WavPlayer  extends JFrame {
-    JButton btn = new JButton("Play Sound");
-    File wavFile;
+    JButton btn ;
+    JButton btn2;
     URL defaultSound;
     public static Clip clip;
     public static AudioInputStream audioInputStream;
-
-    public WavPlayer(String url) {
+        JFrame frame;
+    public WavPlayer()  {
+        btn = new JButton("Play Sound");
+        btn2= new JButton("Stop ");
         try {
+//        btn.setBounds(45, 100, 120, 50);
+//        btn.setBounds(45, 100, 120, 50);
             setSize(300, 100);
             setLocation(400, 300);
             setDefaultCloseOperation(EXIT_ON_CLOSE);
             JPanel jp = new JPanel();
-            defaultSound = new URL(url);
-
+            defaultSound = new URL("1.wav");
             jp.add(btn);
-
+            jp.add(btn2);
             getContentPane().add(jp);
             pack();
-            btn.addActionListener(new ActionListener() {
-                @Override
+                       btn.addActionListener(new ActionListener() {
+               @Override
                 public void actionPerformed(ActionEvent e) {
                     play();
                 }
             });
-        } catch (MalformedURLException ex) {
-            Logger.getLogger(WavPlayer.class.getName()).log(Level.SEVERE, null, ex);
+       } catch (MalformedURLException ex) {
+           Logger.getLogger(WavPlayer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
     public void play() {
@@ -58,9 +60,14 @@ public class WavPlayer  extends JFrame {
         }
     }
     public void stop() {
-        clip.stop();
+        btn2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                clip.stop();
+            }
+        });
+
     }
-
-
 }
+
 
