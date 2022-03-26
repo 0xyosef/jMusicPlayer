456378cf90571c5d738b3d3ba1ab75d3135dd48d
coddinglearn <89706606+coddinglearn@users.noreply.github.com>
2022-03-26 02:33:52 +0200

    Add Simple GUI and Some Features

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,23 @@
 package com.company;
 
+import javax.swing.*;
 import java.net.MalformedURLException;
 
 public class Main {
 
-    public static void main(String[] args) {
-        WavPlayer t = new WavPlayer();
-        t.setVisible(true);
+    public static void main(String[] args) throws MalformedURLException {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                new SwingAudioPlayer().setVisible(true);
+            }
+        });
     }
 }
---
+++ src/com/company/PlayingTimer.java
@@ -0,0 +1,80 @@
+package com.company;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import javax.sound.sampled.Clip;
+import javax.swing.JLabel;
+import javax.swing.JSlider;
+
+public class PlayingTimer extends Thread {
+    private DateFormat dateFormater = new SimpleDateFormat("00:00:00");
+    private boolean isRunning = false;
+    private boolean isPause = false;
+    private boolean isReset = false;
+    private long startTime;
+    private long pauseTime;
+
+    private JLabel labelRecordTime;
+    private JSlider slider;
+    private Clip audioClip;
+
+    public void setAudioClip(Clip audioClip) {
+        this.audioClip = audioClip;
+    }
+
+    public PlayingTimer(JLabel labelRecordTime, JSlider slider) {
+        this.labelRecordTime = labelRecordTime;
+        this.slider = slider;
+    }
+
+    public void run() {
+        isRunning = true;
+
+        startTime = System.currentTimeMillis();
+
+        while (isRunning) {
+            try {
+                Thread.sleep(100);
+                if (!isPause) {
+                    if (audioClip != null && audioClip.isRunning()) {
+                        labelRecordTime.setText(toTimeString());
+                        int currentSecond = (int) audioClip.getMicrosecondPosition() / 1_000_000;
+                        slider.setValue(currentSecond);
+                    }
+                } else {
+                    pauseTime += 100;
+                }
+            } catch (InterruptedException ex) {
+                ex.printStackTrace();
+                if (isReset) {
+                    slider.setValue(0);
+                    labelRecordTime.setText("00:00:00");
+                    isRunning = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    public void reset() {
+        isReset = true;
+        isRunning = false;
+    }
+
+    public void pauseTimer() {
+        isPause = true;
+    }
+
+    public void resumeTimer() {
+        isPause = false;
+    }
+    private String toTimeString() {
+        long now = System.currentTimeMillis();
+        Date current = new Date(now - startTime - pauseTime);
+        dateFormater.setTimeZone(TimeZone.getTimeZone("GMT"));
+        String timeCounter = dateFormater.format(current);
+        return timeCounter;
+    }
+}
